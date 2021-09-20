c82e08f282f739f7fc8df8218eba9a37dc1e7b19
Yan <66186294+iMartin111@users.noreply.github.com>
2021-09-19 22:25:09 -0300

    Add files via upload

---
+++ Clima-iOS13/Clima/Model/WeatherModel.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Yan Akhrameev on 18/09/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    // MARK: - Properties:
+    
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    var conditionName: String {
+        switch conditionId {
+            case 200...232:
+                return "cloud.bolt"
+            case 300...321:
+                return "cloud.drizzle"
+            case 500...531:
+                return "cloud.rain"
+            case 600...622:
+                return "cloud.snow"
+            case 701...781:
+                return "cloud.fog"
+            case 800:
+                return "sun.max"
+            case 801...804:
+                return "cloud.bolt"
+            default:
+                return "cloud"
+        }
+    }
+    
+}
